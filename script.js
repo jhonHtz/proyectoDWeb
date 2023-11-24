59881ad5a7ebb04936b097cf3dc36e630df378f3
JhonHtz <jonatanhtz@outlook.com>
2023-11-24 09:29:30 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleModoNocturno() {
+    document.body.classList.toggle('nocturno');
+
+    document.getElementById('html').classList.toggle('nocturno');
+    document.getElementById('css').classList.toggle('nocturno');
+    document.getElementById('js').classList.toggle('nocturno');
+}
+
